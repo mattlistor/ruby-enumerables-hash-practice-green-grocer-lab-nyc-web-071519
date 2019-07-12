f8e6abbbbfd77e52c7f2e96b4d6f83099e3eb1c7
Matthew Listor <mattlistor@gmail.com>
2019-07-12 03:08:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,5 +48,7 @@ def checkout(items, coupons)
   cart2.each do |name, price_hash|
     total += price_hash[:price] * price_hash[:count]
   end
-  total > 100 ? total * 0.9 : total
+  if total > 100 return total * 0.9
+  else return total 
+  end
 end
