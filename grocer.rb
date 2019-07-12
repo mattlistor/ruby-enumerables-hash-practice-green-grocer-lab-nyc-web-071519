f1f45dd5e185e4301324f35e673f930118570741
Matthew Listor <mattlistor@gmail.com>
2019-07-12 03:28:57 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,58 @@
 def consolidate_cart(cart)
-  # code here
+  newCart = {}
+  cart.each do |itemHash|
+    itemHash.each do |item, descriptionHash| 
+      if newCart.include?(item) == false
+        newCart[item] = descriptionHash
+        newCart[item][:count] = 1
+      else
+        newCart[item][:count] += 1
+      end
+    end
+  end
+  return newCart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart, coupons) 
+  coupons.each do |couponHash| 
+    item = couponHash[:item] 
+    if cart.include?(item) && cart[item][:count] >= couponHash[:num] 
+      cart[item][:count] = cart[item][:count] - couponHash[:num] 
+      if cart.include?(item + " W/COUPON")
+        cart[item + " W/COUPON"][:count] += couponHash[:num] 
+      else 
+        newPrice = couponHash[:cost]/couponHash[:num]
+        cart[item + " W/COUPON"] = {:price => newPrice, 
+        :clearance => cart[item][:clearance], :count => couponHash[:num]} 
+      end 
+    end 
+  end 
+  return cart 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, priceHash|
+    if priceHash[:clearance] == true
+      priceHash[:price] -= (priceHash[:price] * 0.2)
+      priceHash[:price] = priceHash[:price].round(2)
+    end
+  end
+  return cart
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(items, coupons)
+  colsolidatedCart = consolidate_cart(items)
+  couponedCart = apply_coupons(colsolidatedCart, coupons)
+  finalCart = apply_clearance(couponedCart)
+  
+  priceTotal = 0
+  finalCart.each do |name, priceHash|
+    priceTotal += priceHash[:price] * priceHash[:count]
+  end
+  
+  if priceTotal > 100 
+    priceTotal-= priceTotal * 0.1
+  end
+  
+  return priceTotal 
 end
